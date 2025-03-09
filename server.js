98c15828c356c13e6589f1fc4dc5961f2f4c8d39
Dipesh Kafle <dipeshkaflen2060@gmail.com>
2025-03-09 22:27:57 +0530

    io emit vs socket emit

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require("path");
 const http = require("http");
 const { Server } = require("socket.io");
 
+// socket.emit:
+// Sends a message to the specific client that initiated the connection.
+// It is used when you want to send a message to a single client.
+
+// io.emit:
+// Sends a message to all connected clients.
+// It is used when you want to broadcast a message to every client connected to the server.
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -12,7 +20,10 @@ app.use(express.static("public"));
 io.on("connection", (socket) => {
   socket.on("user-message", (message) => {
     console.log(message);
-    io.emit("everyone", message);
+    //io.emit("eveyone") emits to everyone
+
+    //this is only send to those servers except the one which is sending
+    socket.broadcast.emit("everyone", message);
   });
 
   console.log("New user Connected");
